2904d060cebf91a47f31c1881f3342d7b4bff124
Harshal Sakhare <harshalsakhare2305@gmail.com>
2026-06-27 16:35:46 +0530

    queryparameter

---
+++ SpringWebMVCTutorial1/src/main/java/org/springboottutorials/controllers/QueryParameterController.java
@@ -0,0 +1,30 @@
+package org.springboottutorials.controllers;
+
+
+import org.springboottutorials.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QueryParameterController {
+
+    @GetMapping("/profile")
+    public String getdetails(Model model){
+        System.out.println("Inside Query Controller");
+         User user =new User("Harshal",22,"Mumbai");
+       // model.addAttribute("msg","Good Morning Harshal , Welcome to Our New SpringBoot Course");
+        model.addAttribute("Profile",user);
+        return "profile";
+    }
+
+    @GetMapping("/profile/{name}")
+    public String getCustomGreetings(@PathVariable("name") String name, Model model){
+        System.out.println("Inside Query Controller");
+       // User user =new User(name,22,"Mumbai");
+         model.addAttribute("msg","Good Morning "+name +" , Welcome to Our New SpringBoot Course");
+       // model.addAttribute("Profile",user);
+        return "customProfile";
+    }
+}
