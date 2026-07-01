94014e8ae2700c6bcbd895860888082c47f0c5b9
Harshal Sakhare <harshalsakhare2305@gmail.com>
2026-07-01 07:25:31 +0530

    unversion file commit

---
+++ CtoBTicketBookingApp/src/main/java/org/ctobticketbookingapp/CtoBTicketBookingAppApplication.java
@@ -0,0 +1,13 @@
+package org.ctobticketbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtoBTicketBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtoBTicketBookingAppApplication.class, args);
+    }
+
+}
---
+++ CtoBTicketBookingApp/src/test/java/org/ctobticketbookingapp/CtoBTicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ctobticketbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtoBTicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
