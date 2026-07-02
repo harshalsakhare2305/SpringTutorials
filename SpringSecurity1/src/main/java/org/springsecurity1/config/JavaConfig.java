1d22dcd5cd19fe6f6f52febdb3298d1373f21aa3
Harshal Sakhare <harshalsakhare2305@gmail.com>
2026-07-02 16:05:47 +0530

    Adding few more configurations

--- SpringSecurity1/src/main/java/org/springsecurity1/config/JavaConfig.java
+++ SpringSecurity1/src/main/java/org/springsecurity1/config/JavaConfig.java
@@ -3,11 +3,11 @@ package org.springsecurity1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.beans.Customizer;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +21,15 @@ public class JavaConfig {
         http.csrf(Customizer-> Customizer.disable());
 
         /*Enable the Auth form*/
+      //  http.formLogin(Customizer.withDefaults());
 
+
+        //Enable access of Application to tools like postman
+        http.httpBasic(Customizer.withDefaults());
+
+
+        //To make Session as Stateless
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
 
